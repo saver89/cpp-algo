0e0332b820d9ab65f6535d9f9f118c9f7532d613
vladimir.nee <neelkim1989@gmail.com>
2024-09-07 15:08:15 +0500

    change folders structure

--- ConsoleTester/lessons/0/string-length.h
+++ ConsoleTester/lessons/0/string-length.h
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "itask.h"
+#include "../../itask.h"
 
 class StringLength : public ITask {
   public:
--- ConsoleTester/main.cpp
+++ ConsoleTester/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "string-length.h"
+#include "lessons/0/string-length.h"
 #include "tester.h"
 
 int main() {
   StringLength task;
-  Tester tester(&task, "tests");
+  Tester tester(&task, "lessons/0/tests");
   tester.RunTest();
   
   return 0;
